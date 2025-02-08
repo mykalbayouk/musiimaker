3f065adac828bc88eb07acf91f7a712495270582
ashiqsyed <ahs57816@uga.edu>
2025-02-07 20:57:32 -0500

    created navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,1 +1,14 @@
 // navbar component
+export default function Navbar() {
+    return(
+        <div className="border border-black w-full h-[50px] flex">
+            <div className="border border-black w-3/4 text-[32px] h-full">Musiimaker</div>
+            <div className="border border-black w-1/4 text-[24px] h-full">
+                <ul className="flex w-full justify-around align-center h-full">
+                    <li className="border-r border-r-black w-1/2 h-full text-center">Log in</li>
+                    <li className="w-1/2 h-full text-center">Sign up</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
