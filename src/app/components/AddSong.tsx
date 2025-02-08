3ea01f9c9a64d7c71b3787296913bf74be180b91
ashiqsyed <ahs57816@uga.edu>
2025-02-08 01:18:00 -0500

    created add song route and component

---
+++ src/app/components/AddSong.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import Navbar from "./Navbar";
+import { useState } from "react";
+export default function AddSong() {
+    interface Song {
+        songName: String,
+        instrument: String,
+        file: any
+    }
+    const [songName, setSongName] = useState("");
+    const [instrument, setInstrument] = useState("");
+    const [file, setFile] = useState("");
+
+    const handleSongNameChange = (event: any) => {
+        setSongName(event.target.value);
+    }
+    const handleInstrumentChange = (event: any) => {
+        setInstrument(event.target.value);
+    }
+    const handleFileChange = (event: any) => {
+        setFile(event.target.value);
+    }
+    const handleUpload = (event: any) => {
+        event.preventDefault();
+        const song: Song = {
+            songName: songName,
+            instrument: instrument,
+            file: "file"
+        }
+       console.log(song)
+    }
+    return (
+        <div>
+            <Navbar />
+            <div className="flex justify-center items-center h-[780px] w-full"> 
+                <div className="h-[400px] w-[400px] flex flex-col justify-center items-center border border-black rounded-[20px]">
+                    <h1 className="text-2xl w-1/4 text-center h-[50px] flex items-center">Add Song</h1>
+                    <form className="h-[300px] flex flex-col w-[390px] items-center justify-center">
+                        <input onChange={handleSongNameChange} type="text" placeholder="Song Name" className="border-b border-b-black outline-none m-2 w-[200px] transition duration-300 focus:shadow-[0_3px_3px_black] h-[35px] p-2 " />
+                        <input onChange={handleInstrumentChange} type="text" placeholder="Instrument" className="border-b border-b-black outline-none m-2 w-[200px] transition duration-300 focus:shadow-[0_3px_3px_black] h-[35px] p-2" />
+                        <input type="file" placeholder="password" className=" m-2 w-[200px]  h-[35px] p-2" />
+                        <button onClick={handleUpload} type="submit" className="border border-black p-2 rounded-full transition duration-150 hover:shadow-[0_3px_3px_black] m-2" >Upload</button>
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,10 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
       <div className={styles.mainDiv}>
         <div></div>
         <Feed />
         <div></div>
-=======
-      <div>
-        feed goes here, unauthenticated users can only view posts
->>>>>>> login
       </div>
     </div>
   );
