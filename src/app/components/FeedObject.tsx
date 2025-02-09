eb436dbd6859c05c683ecc682af17d245f7a93d1
JacksonLGreer <jlg40585@uga.edu>
2025-02-09 04:35:15 -0500

    feed has pdf

--- src/app/components/FeedObject.tsx
+++ src/app/components/FeedObject.tsx
@@ -6,13 +6,39 @@ import styles from "./FeedObject.module.css";
 
 export default function FeedObject(props: any) {
 
+    
+    const pdfFile = stringToPdf(props.song.file);
     return(
         <div className={styles.feedObj} onClick={() => props.handleSongClick(props.song._id)}>  
                 <div className={styles.top}>
                     <p>{props.song.title}</p>
                     <p className={styles.right}>{props.song.username}</p> 
                 </div>                
-            <img src={props.song.song_file} alt={props.song.title}></img>
+                <embed
+                    src={URL.createObjectURL(pdfFile)}
+                        type="application/pdf"
+                        width="100%"
+                        height="100%"
+                />
         </div>
     )
 }
+
+/**
+ * Takes in a base64 string and returns a file
+ * @param base64 
+ * @returns 
+ */
+export function stringToPdf(base64: string): File {
+    if (!base64) {
+        return new File([], 'output.pdf', { type: 'application/pdf' });
+    }
+    const byteString = atob(base64.split(',')[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], { type: 'application/pdf' });
+    return new File([blob], 'output.pdf', { type: 'application/pdf' });
+  }
